d3eef165dbea90dbc6f0f4de3daf317dedf86ae2
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-04-03 13:45:21 +0100

    Trying again docstring not being seen

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -2,4 +2,3 @@
 """Another document string for the file don't know why
 It's currently not passind the checkerrrrrrr!
 """
-
--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Here application initialization begins
-"""
+"""Here application initialization begins: Create an app instance"""
 
 from flask import Flask, jsonify
 from flask_cors import CORS
@@ -20,18 +18,13 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception):
-    """
-    teardown function
-    """
+    """Function that's run after each request is handled"""
     storage.close()
 
 
 @app.errorhandler(404)
 def handle_404(exception):
-    """
-    handles 404 error
-    :return: returns 404 json
-    """
+    """Handles the 404 Exception when the app encounters a 404"""
     data = {
         "error": "Not found"
     }
@@ -39,7 +32,7 @@ def handle_404(exception):
     resp = jsonify(data)
     resp.status_code = 404
 
-    return(resp)
+    return resp
 
 
 if __name__ == "__main__":
